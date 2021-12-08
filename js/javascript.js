7afa0dbbbeaf3264e98dcf46c22cd287f36332df
Xyrec <itsgonnabealongnight@gmail.com>
2021-12-08 22:15:16 +0100

    bestilling og kvittering

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 console.log("JS loaded");
 
-// Burger-menu
+// Burger-menu kode //
 // Sætter constants, så det er nemmere at kalde på senere
 const burgerbtn = document.getElementById('navburger');
 const burgermenu = document.getElementById('burgermenu');
@@ -10,9 +10,9 @@ let burgermenuOpen = false;
 console.log(!burgermenuOpen)
 
 // Hvis der trykkes på "burgerbtn" (constant vi satte tidligere)
-burgerbtn.addEventListener("click", function(event) {
-    event.preventDefault(); // Ignorer den event der normalt vil ske, ved klik af knappen
-    console.log("Burger button clicked") // Debugging, logger i console at der er trykket
+burgerbtn.addEventListener("click", function (event) {
+    event.preventDefault(); // Ignorer den event der normalt vil ske, og i stedet...
+
     if (!burgermenuOpen) { // Hvis burgermenu er lukket
         burgerbtn.classList.add('open'); // Tilføj en class "open" til elementet navburger
         burgermenuOpen = true; // Sæt burgermenuOpen til true (åben)
@@ -26,3 +26,44 @@ burgerbtn.addEventListener("click", function(event) {
         burgermenu.style.display = "block"; // Åbner menuen hvis den er lukket
     }
 })
+
+// Kode til booking og kvitteringsside //
+// Sætter constants op, så vi kan referere til specifikke ID'er hurtigere
+const booking = document.getElementById('booking');
+const bookingDate = document.getElementById('bookingDate');
+const bookingTime = document.getElementById('bookingTime');
+const bookingName = document.getElementById('bookingName');
+const bookingEmail = document.getElementById('bookingEmail');
+const bookingTele = document.getElementById('bookingTele');
+const bookingComment = document.getElementById('bookingComment');
+const kvittering = document.getElementById('kvittering');
+
+// Hvis et element med ID booking eksisterer, så
+if (booking) {
+    booking.addEventListener("submit", function (event) { // Hvis form modtager "submit" signal
+        event.preventDefault(); // Ignorer den event der normalt vil ske, og i stedet...
+
+        // Sender brugeren videre til kvitteringssiden med indtastede informationer i URL
+        window.location.href = `kvittering.html?dato=${bookingDate.value}&tid=${bookingTime.value}&navn=${bookingName.value}&email=${bookingEmail.value}&telefon=${bookingTele.value}&kommentar=${bookingComment.value}`;
+    })
+}
+
+// Hvis et element med ID kvittering eksisterer, så
+if (kvittering) {
+    const urlData = new URLSearchParams(window.location.search); // Henter alle data fra URL
+
+    // Indsætter alle indtastede informationer i de rette felter på kvitteringssiden
+    bookingDate.insertAdjacentText("beforeend", urlData.get("dato"));
+    bookingTime.insertAdjacentText("beforeend", urlData.get("tid"));
+    bookingName.insertAdjacentText("beforeend", urlData.get("navn"));
+    bookingEmail.insertAdjacentText("beforeend", urlData.get("email"));
+    bookingTele.insertAdjacentText("beforeend", urlData.get("telefon"));
+    bookingComment.insertAdjacentText("beforeend", urlData.get("kommentar"));
+
+    // Normalt ville disse informationer også blive gemt i en database som Stine kan tilgå, 
+    // men da vi ikke har haft undervisning som databaser endnu, 
+    // kan vi ikke fortroligt gemme brugers informationer.
+    // Vi kunne gemme data til en JSON-fil, men den kan tilgås af alle når hjemmesiden
+    // kommer til at ligge ude på nettet.
+    // Den nuværende løsning gemmer ikke informationer på lang sigt.
+}
